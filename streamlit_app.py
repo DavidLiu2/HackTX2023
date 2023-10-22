c53d2519c932dff06339fc707a0d57171faa014d
David Liu <44784993+DavidLiu2@users.noreply.github.com>
2023-10-22 14:53:37 +0000

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ import streamlit as st
 
 item_type = st.radio(
     "Item Type",
-    ["Food :knife_fork_plate:", "Toy :teddy_bear:", "Enclosure :house: ", "Bed :bed:", "Cleaning :soap:", "Fish Tank :fish:"])
+    ["Food :knife_fork_plate:", "Toy :teddy_bear:", "Enclosure :house:", "Bed :bed:", "Cleaning :soap:", "Fish Tank :fish:"])
 item_quantity = st.number_input("Quantity", min_value=1)
 item_price = st.number_input("Price", min_value=0.01)
 
@@ -57,10 +57,56 @@ if st.button("Clear Form"):
 
 
 
+# Parameters for the normal distribution
+mean = 0
+std_dev = 1
+sample_size = 200
+desired_percentile = 25
 
-arr = np.random.normal(1, 1, size=100)
+if item_type == "Food :knife_fork_plate:":
+    mean = 10
+    std_dev = 3
+if item_type == "Toy :teddy_bear:":
+    mean = 2
+    std_dev = 10
+if item_type == "Enclosure :house:":
+    mean = 100
+    std_dev = 100
+    desired_percentile = 40
+if item_type == "Bed :bed:":
+    mean = 20
+    std_dev = 10
+if item_type == "Cleaning :soap:":
+    mean = 7
+    std_dev = 3
+if item_type == "Fish Tank :fish:":
+    mean = 200
+    std_dev = 100
+    desired_percentile = 10
+
+# Generate a normal distribution
+normal_data = np.random.normal(mean, std_dev, sample_size)
+
+# Calculate the threshold value for the desired percentile
+threshold = np.percentile(normal_data, desired_percentile)
+
+if threshold < 0:
+    threshold = 1
+
+# Filter the data to retain values above the threshold
+filtered_data = normal_data[normal_data >= threshold]
+
+
 fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
+ax.hist(filtered_data, bins=20)
+
+ax.axvline(x=item_price, color='red', linestyle='--', label=f'Item Price: {item_price}')
+ax.legend()
+
+# Customize the plot as needed (e.g., labels, titles, etc.)
+ax.set_xlabel('Price')
+ax.set_ylabel('Frequency')
+ax.set_title('Your Item Price Compared')
 
 fig  # 👈 Draw a Matplotlib chart
 
